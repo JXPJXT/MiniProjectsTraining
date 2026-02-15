3bea2829023e14ab9a01d202a53d2941b44415b2
Japjot Singh Bhatia <samjj317@gmail.com>
2026-02-15 16:06:17 +0530

    va

--- CodesR/dp/ClimbStairs.java
+++ CodesR/dp/ClimbStairs.java
@@ -8,6 +8,7 @@ public class ClimbStairs {
         int[] ways = new int[n + 1];
         Arrays.fill(ways, -1);
         System.out.println(countWayMemoization(n, ways));
+        System.out.println(countWaysTab(n));
     }
 
     public static int countWaysRecursive(int n) {
@@ -32,5 +33,19 @@ public class ClimbStairs {
         }
         ways[n] = countWayMemoization(n - 1, ways) + countWayMemoization(n - 2, ways);
         return ways[n];
+    }
+
+    public static int countWaysTab(int n) {
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
